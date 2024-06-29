eb73919a28407d61f46eee3cc55dc0aa5b9b1d97
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2024-06-30 00:38:55 +0100

    commit

--- src/components/Main/CoverFlow.jsx
+++ src/components/Main/CoverFlow.jsx
@@ -1,46 +1,44 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import { EffectCoverflow, Pagination, Navigation, Autoplay } from 'swiper';
+import { EffectCoverflow, Pagination, Navigation, Autoplay } from "swiper";
 
 // Import images from the web
-import slide_image_1 from "../../assets/Photos/codefest (1).jpg"
-import slide_image_2 from "../../assets/Photos/codefest (2).jpg"
-import slide_image_3 from "../../assets/Photos/codefest (3).jpg"
-import slide_image_4 from "../../assets/Photos/codefest (4).jpg"
-import slide_image_5 from "../../assets/Photos/codefest (5).jpg"
-import slide_image_6 from "../../assets/Photos/codefest (6).jpg"
-import slide_image_7 from "../../assets/Photos/codefest (7).jpg"
-import slide_image_8 from "../../assets/Photos/codefest (8).jpg"
+import slide_image_1 from "../../assets/Photos/codefest (1).jpg";
+import slide_image_2 from "../../assets/Photos/codefest (2).jpg";
+import slide_image_3 from "../../assets/Photos/codefest (3).jpg";
+import slide_image_4 from "../../assets/Photos/codefest (4).jpg";
+import slide_image_5 from "../../assets/Photos/codefest (5).jpg";
+import slide_image_6 from "../../assets/Photos/codefest (6).jpg";
+import slide_image_7 from "../../assets/Photos/codefest (7).jpg";
+// import slide_image_8 from "../../assets/Photos/codefest (8).jpg";
 
-
-import './coverflow.css';
+import "./coverflow.css";
 
 function Coverflow() {
   return (
     <div className="ccontainer">
-
       <Swiper
-        effect={'coverflow'}
+        effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
         loop={true}
-        slidesPerView={'auto'}
+        slidesPerView={"auto"}
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
           depth: 100,
           modifier: 2.5,
         }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
         navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
           clickable: true,
         }}
         modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
@@ -68,8 +66,6 @@ function Coverflow() {
         <SwiperSlide>
           <img src={slide_image_7} alt="slide_image" />
         </SwiperSlide>
-
-  
       </Swiper>
     </div>
   );
